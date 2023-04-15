9993eba056292e78daada9b8eecefb9038832bc8
Ilona <ppsport88@mail.ru>
2023-04-15 16:24:34 +0300

    16

--- Task16.py
+++ Task16.py
@@ -7,10 +7,9 @@
 n = int(input('Введите размер элементов списка: '))
 list_n = input('Введите элементы списка через пробел: ').split()
 arr = list(map(int, list_n))
+x = int (input('Введите число х: '))
+count = 0
 for i in range(n):
-    x = int (input('Введите число х: '))
-    count = 0
-    for i in range(n):
-        if arr[i] == x:
-            count += 1
-    print(f'Число {x} встречается в списке А {count} раз.')
+    if arr[i] == x:
+        count += 1
+print(f'Число {x} встречается в списке А {count} раз.')
